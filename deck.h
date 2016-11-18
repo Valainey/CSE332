eeac91f00d949fe9db2d6aa4347ca24eb6d090b0
Cindy L <TCXX@users.noreply.github.com>
2016-11-18 14:46:20 -0600

    Update deck.h

--- deck.h
+++ deck.h
@@ -16,67 +16,28 @@
 //for std::copy 
 #include <iterator> 
 
-/*#include "stdafx.h"
-#include "card.h"
-#include "hand.h"
-#include <vector>
-#include <iostream>
-class hand;
-using namespace std;
-
-
-
-class Deck {
-	friend void operator<<(Hand&, Deck&);
-	friend const bool & operator<< (const ostream &, const Deck &);
-	friend bool check_valid(string word);
-	int deck_size;
-	vector<Card> cards;
-	void Deck::shuffle();
-	void add_card(Card&);
-
-public:
-	Deck() = default;
-	Deck(const char* &filename);
-
-	//compile should be fine
-	//vector can contain the memory
-	int load(const char* filename);
-	int size();
-private:
-	std::vector<Card> cards;
-	Deck(const Deck& d) = delete;
-
-};
-
-//void start(Deck& h);
-void operator<<(Hand& h, Deck& d);
-
-
-//std::ostream& operator<<(std::ostream& out, const Deck& deck);*/
-
 using namespace std;
 class Hand;
 
 class Deck
 {
-public:
-Deck() = default;
-Deck(const char* &filename);
-~Deck();
+	public:
+	Deck() = default;
+	Deck(const char* &filename);
+	~Deck();
 
-vector<Card> getCards() const;
-Card popCard();
-void shuffle();
-void add_card(Card&c);
-int size() const;
-string toString() const;
+	vector<Card> getCards() const;
+	Card popCard();
+	void shuffle();
+	void addCard(Card&c);
+	int size() const;
+	string toString() const;
 
-private:
-int load(const char* &filename);
-vector<Card> cards;
-CardSuit convertSuit(const char& input);
-CardRank convertRank(const char& input);
+	private:
+	int load(const char* &filename);
+	vector<Card> cards;
+	CardSuit convertSuit(const char& input);
+	CardRank convertRank(const char& input);
 
 };
 
